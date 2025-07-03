ec97269c4160f6c2193a8eae277227370a4bc60b
Rushal69 <rushalchaurasia2020@gmail.com>
2025-07-04 00:17:21 +0530

    Revert Landing Page to Original State

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,5 @@
 import { defineConfig } from 'vite'
-import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default defineConfig({
-  plugins: [
-    createHtmlPlugin({
-      minify: true,
-    })
-  ],
-  build: {
-    minify: 'terser',
-    cssCodeSplit: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['webfont']
-        }
-      }
-    }
-  }
+  // Basic Vite configuration
 })
